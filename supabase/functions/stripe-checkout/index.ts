df3e01abf8a315a7c557602c675cbaf1dba21175
haysally19 <haydencolesalyer@gmail.com>
2026-01-14 13:32:33 -0500

    Updated index.ts

--- supabase/functions/stripe-checkout/index.ts
+++ supabase/functions/stripe-checkout/index.ts
@@ -15,8 +15,8 @@ const stripe = new Stripe(stripeSecret, {
 function corsResponse(body: string | object | null, status = 200) {
   const headers = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Client-Info, apikey',
   };
 
   // For 204 No Content, don't include Content-Type or body
