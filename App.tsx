b64d90aa0b038ef0847273c9c1935558a42dd2ae
haysally19 <haydencolesalyer@gmail.com>
2026-01-26 16:20:49 -0500

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -13,7 +13,6 @@ import ClassView from './components/ClassView';
 import CalendarView from './components/CalendarView';
 import SettingsView from './components/SettingsView';
 import PaywallModal from './components/PaywallModal';
-import CameraSolver from './components/CameraSolver';
 import QuickSolve from './components/QuickSolve';
 import { Menu } from 'lucide-react';
 import Logo from './components/Logo';
@@ -169,7 +168,6 @@ const MainLayout: React.FC<MainLayoutProps> = (props) => {
                 <Routes>
                 <Route path="/" element={<Dashboard courses={props.courses} streak={props.streak} userTier={props.userTier} tierLoaded={props.tierLoaded} onUpgrade={props.onUpgrade} onAddCourse={props.onAddCourse} />} />
                 <Route path="/quick-solve" element={<QuickSolve checkTokenLimit={props.checkTokenLimit} incrementTokenUsage={props.incrementTokenUsage} onNavigateToDashboard={() => window.location.hash = '#/'} />} />
-                <Route path="/solver" element={<CameraSolver />} />
                 <Route
                     path="/classes"
                     element={
